1a4a652128aa5793d87544a5c1d421930556d84e
James Funk <karjamfunk@gmail.com>
2026-02-21 22:34:18 -0500

    Initial Cadenza homepage

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main style={{
+      display: "flex",
+      height: "100vh",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>Welcome to Cadenza</h1>
+    </main>
+  );
+}
